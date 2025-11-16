3a1c78c33ff66cf20c860079b466e3eef9d4e4df
Vilius Vilimas <vilius.vilimas@stud.viko.lt>
2025-11-16 16:53:38 +0200

    Kuriamas Login

--- WindowsFormsApp/Login.Designer.cs
+++ WindowsFormsApp/Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Required designer variable.
@@ -29,11 +29,11 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -45,20 +45,6 @@
             this.button1.Text = "Log In";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(338, 182);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(338, 148);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 2;
             // 
             // label1
             // 
@@ -90,16 +76,32 @@
             this.label3.Text = "Slaptažodis";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(349, 151);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(349, 185);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 443);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -112,11 +114,11 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
---
+++ WindowsFormsApp/Login.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp
+{
+
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        } 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connStr = "Server=localhost;Database=studentis;Uid=root;Pwd=";
+            string sql = "SELECT COUNT(1) FROM naudotojas WHERE vardas=@Vardas AND pass=@Pass";
+            string sqlRole = "SELECT role FROM naudotojas WHERE vardas=@Vardas AND pass=@Pass";
+
+
+            using (var con = new MySqlConnection(connStr))
+            using (var cmd = new MySqlCommand(sql, con))
+            {
+                cmd.Parameters.Add("@Vardas", MySqlDbType.VarChar).Value = textBox1.Text;
+                cmd.Parameters.Add("@Pass", MySqlDbType.VarChar).Value = textBox2.Text;
+
+                con.Open();
+                int found = Convert.ToInt32(cmd.ExecuteScalar());
+                if (found > 0)
+                {
+
+                    using (var cmdRole = new MySqlCommand(sqlRole, con))
+                    {
+
+                        cmdRole.Parameters.Add("@Vardas", MySqlDbType.VarChar).Value = textBox1.Text;
+                        cmdRole.Parameters.Add("@Pass", MySqlDbType.VarChar).Value = textBox2.Text;
+
+                        if (cmdRole.ExecuteScalar().ToString() == "admin")
+                        {
+                            MessageBox.Show("Successfully logged in");
+                            var adminForm = new AdminView();
+                            adminForm.Show();
+                            return;
+                        }
+                        else if (cmdRole.ExecuteScalar().ToString() == "destytojas")
+                        {
+                            MessageBox.Show("Successfully logged in");
+                            var destytojasForm = new DestytojoView();
+                            destytojasForm.Show();
+                            return;
+                        }
+                        else if (cmdRole.ExecuteScalar().ToString() == "studentas")
+                        {
+                            MessageBox.Show("Successfully logged in");
+                            var studentasForm = new StudentoView();
+                            studentasForm.Show();
+                            return;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Role not recognized");
+
+                        }
+                    }
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Invalid username or password");
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+   
+    interface IRole
+    {
+        void Prisijungti();
+        void Atsijungti();
+        void KeistiSlaptazodi();
+    }
+    class Naudotojas
+    {
+        private
+            string vardas;
+            string pavarde;
+            string pass;
+            int role;
+        public
+             string Vardas { get; set; }
+             string Pavarde { get; set; }
+             string Pass { get; set; }
+             int Role { get; set; }
+
+    }
+    class Studentas : Naudotojas
+    {
+        private
+            int studentID;
+            string grupe;
+
+        public
+            int StudentID { get; set; }
+            string Grupe { get; set; }
+    }
+    class Destytojas : Naudotojas
+    {
+        private
+            int destytojasID;
+            string dalykas;
+        public
+            int DestytojasID { get; set; }
+            string Dalykas { get; set; }
+    }
+    class Administratorius : Naudotojas
+    {
+        private
+            int adminID;
+        public
+            int AdminID { get; set; }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
